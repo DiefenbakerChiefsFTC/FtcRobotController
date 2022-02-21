5944a646feed9f674ae6a71c7a9eb20846f69427
vityism <wz2684@gmail.com>
2022-02-20 20:42:17 -0700

    i really hope this works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/experimentalLinSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/experimentalLinSlide.java
@@ -11,52 +11,7 @@ import org.firstinspires.ftc.teamcode.Tele.untested.linSlide;
 public class experimentalLinSlide extends LinearOpMode {
 
     public DcMotor LinSlideMotor;
-
-    static final int low = 0; //encoder values
-    static final int high = 2100;
-
-    public enum states{LOW, HIGH} //states the slide can be in
-    static linSlide.states state = linSlide.states.LOW;
-
-    public static void setLSMotor(float LTrigger, float RTrigger, DcMotor LSMotor){
-
-        moveLS(LTrigger, RTrigger, LSMotor);
-
-        if(LSMotor.getCurrentPosition() == low){
-            LSMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-    }
-
-    public static void moveLS(float LTrig, float RTrig, DcMotor LSM){
-        switch (state){
-            case LOW:
-                if(LTrig == 1 && LSM.getCurrentPosition() != high){
-                    state = linSlide.states.HIGH;
-                    LSM.setTargetPosition(high);
-                    while (LSM.getCurrentPosition() != high && RTrig != 1) {
-                        LSM.setPower(0.9);
-                        LSM.getCurrentPosition();
-                    }
-
-                }
-                LSM.setPower(0);
-                break;
-            case HIGH:
-                if(RTrig == 1 && LSM.getCurrentPosition() != low){
-                    state = linSlide.states.LOW;
-                    LSM.setTargetPosition(low);
-                    while (LSM.getCurrentPosition() != low && LTrig != 1) {
-                        LSM.setPower(0.9);
-                        LSM.getCurrentPosition();
-                    }
-
-                }
-                LSM.setPower(0.1);
-                break;
-            default:
-                break;
-        }
-    }
+    int STATE = 0;
 
     public void runOpMode() throws InterruptedException {
 
@@ -70,10 +25,21 @@ public class experimentalLinSlide extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-           setLSMotor(gamepad1.left_trigger, gamepad1.right_trigger, LinSlideMotor);
-            // linSlide.setLSMotor(gamepad1.left_trigger, gamepad1.right_trigger, LinSlideMotor);
+            linSlide.mainLSMethod(STATE, gamepad1.left_trigger, gamepad1.right_trigger, LinSlideMotor);
+            sendCurrentState();
 
         }
+
+    }
+
+    public void sendCurrentState() { //takes the state of the lin slide and saves it as a int value for the next cycle of the while loop
+        if (linSlide.sendStateToOPMode() == linSlide.states.LOW) {
+            STATE = 0;
+        } else if (linSlide.sendStateToOPMode() == linSlide.states.HIGH) {
+            STATE = 1;
+        } else if (linSlide.sendStateToOPMode() == linSlide.states.GOUP) {
+            STATE = 2;
+        } else STATE = 3;
 
     }
 
