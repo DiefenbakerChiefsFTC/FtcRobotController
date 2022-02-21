a865e4d6f0fc3384dc12d02f1bf8d9f34178eabb
vityism <wz2684@gmail.com>
2022-02-20 20:54:51 -0700

    i really hope this works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/linSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/linSlide.java
@@ -16,9 +16,9 @@ public class linSlide {
         receiveCurrentState(receivedState);
         moveLS(LTrigger, RTrigger, LSMotor);
 
-        if(LSMotor.getCurrentPosition() == low){
+     /*   if(LSMotor.getCurrentPosition() == low){
             LSMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
+        } */
     }
 
     public static void moveLS(float LTrig, float RTrig, DcMotor LSM){ //moves the lin slide by setting the motor power
