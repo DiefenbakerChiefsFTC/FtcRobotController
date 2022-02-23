bb89c544c621f23ae1fa022d7206ae90fbd69bca
DiefenbakerChiefsFTC <dief.chiefs20802@gmail.com>
2022-02-22 23:03:52 -0700

    Encoder Testing Update 5

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LST.java
@@ -4,16 +4,16 @@ import static com.qualcomm.robotcore.hardware.DcMotor.*;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 
 @TeleOp(name="encoderTest", group="experimental")
 public class LST extends LinearOpMode {
 
-    public DcMotor LinSlideMotor;
+    public DcMotorEx LinSlideMotor;
     public void runOpMode() throws InterruptedException {
 
-        LinSlideMotor = hardwareMap.dcMotor.get("LinSlideMotor");
+        LinSlideMotor = (DcMotorEx) hardwareMap.dcMotor.get("LinSlideMotor");
         LinSlideMotor.setMode(RunMode.STOP_AND_RESET_ENCODER);
         waitForStart();
 
@@ -21,8 +21,8 @@ public class LST extends LinearOpMode {
 
         while (opModeIsActive()) {
             LinSlideMotor.setTargetPosition(1000);
-            LinSlideMotor.setPower(.5);
             LinSlideMotor.setMode(RunMode.RUN_TO_POSITION);
+            LinSlideMotor.setVelocity(100);
             //CurrentPos = LinSlideMotor.getCurrentPosition(); //uncomment this line and comment line 18 if reading returns 0
             telemetry.addData("Encoder Value", LinSlideMotor.getCurrentPosition());
             telemetry.update(); //updates telemetry each cycle.
