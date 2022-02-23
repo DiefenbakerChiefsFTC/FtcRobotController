7a655d24b955f0ea547ada59f3d8f59079c6d31a
vityism <wz2684@gmail.com>
2022-02-22 20:44:00 -0700

    encoder tester method

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LST.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Tele.experimental;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Tele.untested.linSlide;
+
+
+@TeleOp(name="encoderTest", group="experimental")
+public class LST extends LinearOpMode {
+
+    public DcMotor LinSlideMotor;
+
+    public void runOpMode() throws InterruptedException {
+
+        LinSlideMotor = hardwareMap.dcMotor.get("LinSlideMotor");
+        LinSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LinSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            LinSlideMotor.setPower(0.1);
+            System.out.println(LinSlideMotor.getCurrentPosition());
+
+        }
+
+    }
+
+}
+
+
