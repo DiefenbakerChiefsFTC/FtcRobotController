3780258dbac8182b3ea5065845095aa0bf58162e
DiefenbakerChiefsFTC <dief.chiefs20802@gmail.com>
2022-02-22 22:05:17 -0700

    Encoder Testing Update 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LST.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.Tele.experimental;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 import org.firstinspires.ftc.teamcode.Tele.untested.linSlide;
 
 
@@ -11,12 +10,12 @@ import org.firstinspires.ftc.teamcode.Tele.untested.linSlide;
 public class LST extends LinearOpMode {
 
     public DcMotor LinSlideMotor;
-    double CurrentPosition;
     public void runOpMode() throws InterruptedException {
 
         LinSlideMotor = hardwareMap.dcMotor.get("LinSlideMotor");
         LinSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LinSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LinSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        double CurrentPos = LinSlideMotor.getCurrentPosition(); //comment this line and uncomment line 28 if reading still returns 0
 
         waitForStart();
 
@@ -24,10 +23,10 @@ public class LST extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            LinSlideMotor.setPower(.1);
-            LinSlideMotor.setTargetPosition(100);
-            CurrentPosition = LinSlideMotor.getCurrentPosition();
-            System.out.println(CurrentPosition);
+            LinSlideMotor.setPower(.5);
+            LinSlideMotor.setTargetPosition(1000);
+            //CurrentPos = LinSlideMotor.getCurrentPosition(); //uncomment this line and comment line 18 if reading returns 0
+            System.out.println(CurrentPos);
 
         }
     }
