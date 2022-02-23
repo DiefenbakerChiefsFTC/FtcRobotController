e5717b74c2c3176244526f555efdd12e29154fcd
Atharve <atharve010@gmail.com>
2022-02-23 11:37:33 -0700

    File management.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LinearSlideTesting/LST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LinearSlideTesting/LST.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Tele.experimental;
+package org.firstinspires.ftc.teamcode.Tele.experimental.LinearSlideTesting;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/LinearSlide/linSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/LinearSlide/linSlide.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Tele.untested;
+package org.firstinspires.ftc.teamcode.Tele.untested.LinearSlide;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
