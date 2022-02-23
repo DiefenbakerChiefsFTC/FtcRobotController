58fb9e993f471382924eb86e511d5f14ddc76e62
DiefenbakerChiefsFTC <dief.chiefs20802@gmail.com>
2022-02-22 22:52:42 -0700

    Encoder Testing Update 5

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/experimental/LST.java
@@ -15,14 +15,13 @@ public class LST extends LinearOpMode {
 
         LinSlideMotor = hardwareMap.dcMotor.get("LinSlideMotor");
         LinSlideMotor.setMode(RunMode.STOP_AND_RESET_ENCODER);
-        LinSlideMotor.setMode(RunMode.RUN_TO_POSITION);
-
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
             LinSlideMotor.setTargetPosition(1000);
+            LinSlideMotor.setMode(RunMode.RUN_TO_POSITION);
             //CurrentPos = LinSlideMotor.getCurrentPosition(); //uncomment this line and comment line 18 if reading returns 0
             telemetry.addData("Encoder Value", LinSlideMotor.getCurrentPosition());
             telemetry.update(); //updates telemetry each cycle.
