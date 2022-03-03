c53fb3a6d36b5ea6f0ff1e4ddecb092c29db56df
dodobird-ui <68713775+dodobird-ui@users.noreply.github.com>
2022-03-02 20:25:10 -0700

    OpMode for intake + initialize servos & modded intake code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/untested/movement.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Auto.untested;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
+
+public class movement {
+    static CRServo intakeServo;
+    static ServoController sCont= null;
+    static Servo initialServo;
+    static ServoController ServoController1;
+
+}
