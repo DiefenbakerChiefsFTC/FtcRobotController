a92f0041e92f11c329e82fbbec47adc31834e5ee
Mcveigth <102336370+mcveigth@users.noreply.github.com>
2022-10-17 16:37:04 -0600

    Update ServoTele.java
    main functionally of servo was implement(pending on decide whether to use 1 servo or 2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/ServoTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/ServoTele.java
@@ -4,13 +4,49 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 
-@TeleOp
-
+//edit if required to adapt to 2 servos
 public class ServoTele extends LinearOpMode {
-    CRServo Claw1;
-    CRServo Claw2;
-    @Override
-    public void runOpMode() throws InterruptedException{
-
+    static Servo ClawL; 
+   // static Servo ClawR;
+    static double startPos = 1;
+    static double closePos = 0.2; // adjust maybe
+    //static servoController sCont = null;
+    static ElapsedTime runTime = null;
+    
+    //initializing servos
+    public static void setServos(Servo Cl/*, Servo Cr*/){
+        ClawL = Cl;
+        //ClawR = Cr;
+        open();
+        
+        sCont = ClawL.getController(); //should declare for both servos
+        
+        sCont.pwmEnable(); //enabling just in case
+        
+        runTime = new ElapsedTime();
+        
+    }
+    
+    //releases object when x is pressed
+    public static void release(boolean x){
+        if(ClawL.getPosition() == -closePos /*&& ClawR.getPosition() == closePos*/ && x){
+             open();  
+        }
+    }
+    //holds object when y is pressed
+    public static void hold(boolean y){
+        if(ClawL.getPosition() == -startPos /*&& ClawR.getPosition() == startPos*/ && y){
+            close();
+        }
+    }
+    
+    public static void open(){
+        ClawL.setPosition(-startPos);
+    //    ClawR.setPosition(startPos);
+    }
+    
+    public static void close(){
+        ClawL.setPosition(-closePos);
+   //     ClawR.setPosition(closePos);
     }
 }
